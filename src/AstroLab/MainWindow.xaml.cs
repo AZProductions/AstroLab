7bc80d14c4bf371fe8cbe18b4433ed2ae13af1e3
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-12-13 15:51:37 +0100

    Started implementing auto-completion.

--- src/AstroLab/MainWindow.xaml.cs
+++ src/AstroLab/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using ModernWpf;
 using ModernWpf.Controls;
 using ModernWpf.Controls.Primitives;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Folding;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Editing;
 
 namespace AstroLab
 {
@@ -9,6 +13,7 @@ namespace AstroLab
     /// </summary>
     public partial class MainWindow : HandyControl.Controls.Window
     {
+        public CompletionWindow completionWindow;
         public MainWindow()
         {
             InitializeComponent();
@@ -18,8 +23,59 @@ namespace AstroLab
             {
                 var newItem = new System.Windows.Controls.TabItem { Header = "New File" };
                 TabItemHelper.SetIcon(newItem, new SymbolIcon(Symbol.Document));
+                ICSharpCode.AvalonEdit.TextEditor Editor = new() {
+                    Margin = new Thickness(10),
+                    FontSize = 16,
+                    Background = Brushes.Transparent,
+                    Foreground = Brushes.White,
+                    FontFamily = new FontFamily("Consolas"),
+                    SyntaxHighlighting = ICSharpCode.AvalonEdit.Highlighting.HighlightingManager.Instance.GetDefinition("C#")
+                    
+                };
+                Editor.TextArea.TextEntering += Editor_TextArea_TextEntering;
+                Editor.TextArea.TextEntered += Editor_TextArea_TextEntered;
+                FoldingManager foldingManager = new FoldingManager(Editor.Document);
+                XmlFoldingStrategy foldingStrategy = new XmlFoldingStrategy();
+                foldingManager = FoldingManager.Install(Editor.TextArea);
+                foldingStrategy = new XmlFoldingStrategy();
+                foldingStrategy.UpdateFoldings(foldingManager, Editor.Document);
+                newItem.Content = Editor;
                 return newItem;
             };
+        }
+
+        private void Editor_TextArea_TextEntered(object sender, TextCompositionEventArgs e)
+        {
+
+            if (e.Text == ".")
+            {
+                TextArea Editor = (TextArea)sender;
+                // Open code completion after the user has pressed dot:
+                completionWindow = new CompletionWindow(Editor);
+                IList<ICompletionData> data = completionWindow.CompletionList.CompletionData;
+                data.Add(new SyntaxCompletionData("Item1"));
+                data.Add(new SyntaxCompletionData("Item2"));
+                data.Add(new SyntaxCompletionData("Item3"));
+                completionWindow.Show();
+                completionWindow.Closed += delegate {
+                    completionWindow = null;
+                };
+            }
+        }
+
+        private void Editor_TextArea_TextEntering(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Length > 0 && completionWindow != null)
+            {
+                if (!char.IsLetterOrDigit(e.Text[0]))
+                {
+                    // Whenever a non-letter is typed while the completion window is open,
+                    // insert the currently selected element.
+                    completionWindow.CompletionList.RequestInsertion(e);
+                }
+            }
+            // Do not set e.Handled=true.
+            // We still want to insert the character that was typed.
         }
     }
 }
---
+++ src/AstroLab/SyntaxCompletionData.cs
@@ -0,0 +1,39 @@
+﻿using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+
+namespace AstroLab
+{
+    public class SyntaxCompletionData : ICompletionData
+    {
+        public SyntaxCompletionData(string text)
+        {
+            this.Text = text;
+        }
+        public double Priority { get; set; }
+
+        public System.Windows.Media.ImageSource Image
+        {
+            get { return null; }
+        }
+
+        public string Text { get; private set; }
+
+        // Use this property if you want to show a fancy UIElement in the list.
+        public object Content
+        {
+            get { return this.Text; }
+        }
+
+        public object Description
+        {
+            get { return "Description for " + this.Text; }
+        }
+
+        public void Complete(TextArea textArea, ISegment completionSegment,
+            EventArgs insertionRequestEventArgs)
+        {
+            textArea.Document.Replace(completionSegment, this.Text);
+        }
+    }
+}
