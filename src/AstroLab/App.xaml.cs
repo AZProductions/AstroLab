a4e492ac3bfea16d34f1915c6c6b1b6034a5719b
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-12-13 14:35:32 +0100

    Added WPF based main project and more.
    - Worked on the UI.
    - Fixed bugs.
    - Referenced Dragablz, HandyControl and ModernWPF.

---
+++ src/AstroLab/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using HandyControl.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AstroLab
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        internal void UpdateSkin(SkinType skin)
+        {
+            SharedResourceDictionary.SharedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(ResourceHelper.GetSkin(skin));
+            Resources.MergedDictionaries.Add(new ResourceDictionary
+            {
+                Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
+            });
+            Current.MainWindow?.OnApplyTemplate();
+        }
+    }
+}
+
---
+++ src/AstroLab/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ModernWpf;
+using ModernWpf.Controls;
+using ModernWpf.Controls.Primitives;
+
+namespace AstroLab
+{
+    /// <summary>
+    /// Main Window.
+    /// </summary>
+    public partial class MainWindow : HandyControl.Controls.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ThemeManager.SetRequestedTheme(window, ElementTheme.Dark);
+            ((App)App.Current).UpdateSkin(HandyControl.Data.SkinType.Dark);
+            TabView.NewItemFactory = () =>
+            {
+                var newItem = new System.Windows.Controls.TabItem { Header = "New File" };
+                TabItemHelper.SetIcon(newItem, new SymbolIcon(Symbol.Document));
+                return newItem;
+            };
+        }
+    }
+}
